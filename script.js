c68650d5d89f747346b2c3d6a884ae159c0a9ddf
Masaki Furuta <masaki.furuta@gmail.com>
2019-04-17 22:03:13 +0900

    Check if google.com

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     var url = tab.url;
-    window.open(url+'&lr=lang_en');
+    var result = url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1];
+    if (result == "www.google.com") {
+        window.open(url+'&lr=lang_en');
+    }
 });
 
