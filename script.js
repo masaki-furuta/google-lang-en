92fada143e6eaab24480816967c30fc0ac61bf9a
Masaki Furuta <masaki.furuta@gmail.com>
2019-11-18 15:54:24 +0900

    Update script.js
    Added www.google.co.jp to  script.js.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     var url = tab.url;
     var result = url.match(/^https?:\/{2,}(.*?)(?:\/|\?|#|$)/)[1];
-    if (result == "www.google.com") {
+    if (result == "www.google.com" || result == "www.google.co.jp") {
         window.open(url+'&lr=lang_en');
     }
 });
